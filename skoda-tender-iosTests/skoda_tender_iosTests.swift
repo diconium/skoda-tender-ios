5842f8f87a9f32e23aca94dd6c58cbd255b49280
cagicser <sergio.cagica@diconium.com>
2024-10-22 10:29:10 +0100

    build: Setup initial Xcode project

---
+++ skoda-tender-iosTests/skoda_tender_iosTests.swift
@@ -0,0 +1,16 @@
+//
+//  skoda_tender_iosTests.swift
+//  skoda-tender-iosTests
+//
+//  Created by Sergio Cagica on 22/10/2024.
+//
+
+import Testing
+
+struct skoda_tender_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
