553793f7470da174d3531c258c3e13a8132e6ded
Fábio Barreiros <fabiojjbarreiros@gmail.com>
2024-10-24 19:34:30 +0100

    feat(notifications): added appDelegate because FCM doesnt work without it and add NotificationHelper

---
+++ skoda-tender-ios/AppDelegate.swift
@@ -0,0 +1,56 @@
+import Firebase
+import FirebaseMessaging
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+    var app: SkodaTenderIosApp?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        self.setupCloudMessaging(application)
+
+        return true
+    }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken as Data
+    }
+
+    private func setupCloudMessaging(_ application: UIApplication) {
+        FirebaseApp.configure()
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { _, _ in }
+        application.registerForRemoteNotifications()
+        UNUserNotificationCenter.current().delegate = self
+        Messaging.messaging().delegate = self
+    }
+}
+
+// MARK: UNUserNotificationCenterDelegate
+extension AppDelegate: UNUserNotificationCenterDelegate {
+
+    // When app is in background notification handler
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+
+        Messaging.messaging().appDidReceiveMessage(userInfo)
+        completionHandler(UIBackgroundFetchResult.newData)
+        UNUserNotificationCenter.current().setBadgeCount(0)
+    }
+
+    // When app is in foreground notification handler
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+
+        completionHandler(.banner)
+    }
+}
+
+// MARK: MessagingDelegate
+extension AppDelegate: MessagingDelegate {
+
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+
+        guard let fcmToken = fcmToken else { return }
+        print("Firebase registration token: \(fcmToken)")
+    }
+}
---
+++ skoda-tender-ios/Shared/Helper/NotificationHelper.swift
@@ -0,0 +1,32 @@
+//
+//  NotificationHelper.swift
+//  skoda-tender-ios
+//
+//  Created by Fábio Barreiros on 24/10/2024.
+//
+
+import Foundation
+import UserNotifications
+
+class NotificationHelper {
+
+    public static func showUserNotification(title: String, message: String = String()) {
+
+        let center = UNUserNotificationCenter.current()
+        center.getDeliveredNotifications { notificationList in
+
+            let titleList = notificationList.compactMap { $0.request.content.title }
+            if !titleList.contains(title) { // only add if it's not already notified
+
+                let content = UNMutableNotificationContent()
+                content.title = title
+                content.body = message
+                content.categoryIdentifier = "alarm"
+                content.sound = UNNotificationSound.default
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                center.add(request)
+            }
+        }
+    }
+}
